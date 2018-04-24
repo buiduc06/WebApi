5c808ff78a50ed3b94985a89347864f6e1d34c98
ducpanda <buiduc1998@gmail.com>
2018-04-25 05:38:33 +0700

    add css for client

--- resources/views/admin/dataajax/data1.blade.php
+++ resources/views/admin/dataajax/data1.blade.php
@@ -1,36 +1,28 @@
 
-   @if(isset($data) && !empty($data) &&count($data)>0)
-<div class="items-wrapper isotope row">
- 
-  @foreach($data as $item)
- <style type="text/css" media="screen">
+@if(isset($data) && !empty($data) &&count($data)>0)
+<style type="text/css" media="screen">
 #imgproduct .img-thumb{
-    background:url({{$item->getImage()}}) no-repeat;
-    background-size:100%;
   display:block;
-  text-align: center;
   padding-top: 20px;
   width:240px;
   height:150px;
-   box-shadow: 1px 3px 20px 5px rgba( 1,2,2,0.05);
-  -webkit-transition: background-position 3s ease-in-out;
-  -moz-transition: background-position 3s ease-in-out;
-  -ms-transition: background-position 3s ease-in-out;
-  -o-transition: background-position 3s ease-in-out;
-  transition: background-position 3s ease-in-out;
-}
-
-#imgproduct .img-thumb:hover {
-  background-position:0px 100%;
+  box-shadow: 1px 3px 20px 5px rgba( 1,2,2,0.05);
 
 }
+
 </style>
+<div class="items-wrapper isotope row">
+
+  @foreach($data as $item)
+  
+  
   <div class="item col-lg-3 col-6 animated flipInX" id="imgproduct" style="cursor: pointer;">
-     <a data-id="{{$item->id}}" class="myproduct-button figure imgproduct img-thumb">
-     {{-- <a class="img-thumb"></a> --}}
-   </a>
-   <div class="item-inner">
-      
+   <a data-id="{{$item->id}}" class="myproduct-button figure imgproduct img-thumb" style="background:url({{$item->getImage()}}) no-repeat;
+   background-size:100%;">
+   {{-- <a class="img-thumb"></a> --}}
+ </a>
+ <div class="item-inner">
+
    <div class="content text-left">
      <h3 class="sub-title"><a href="#">{{ $item->name}}</a></h3>
      <div class="meta">{{ $item->meta !=null ?$item->meta:''}}</div>
@@ -41,7 +33,7 @@
 </div><!--//item-->
 
 @endforeach
- 
+
 </div><!--//item-wrapper-->
 
 @else
