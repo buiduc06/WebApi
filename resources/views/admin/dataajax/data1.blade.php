a49a7bf2dcc426e5a3e5b04b5d4128d7a21ca0b7
ducpanda <buiduc1998@gmail.com>
2018-04-25 04:13:17 +0700

    aaaaa

--- resources/views/admin/dataajax/data1.blade.php
+++ resources/views/admin/dataajax/data1.blade.php
@@ -1,12 +1,33 @@
    @if(isset($data) && !empty($data) &&count($data)>0)
+   <style type="text/css" media="screen">
+  #imgproduct .img-show{
+    background:url({{$item->getImage()}})no-repeat;
+    background-size:100%;
+    display:block;
+    text-align: center;
+    padding-top: 20px;
+    width:380px;
+    height:350px;
+    -webkit-transition: background-position 3s ease-in-out;
+    -moz-transition: background-position 3s ease-in-out;
+    -ms-transition: background-position 3s ease-in-out;
+    -o-transition: background-position 3s ease-in-out;
+    transition: background-position 3s ease-in-out;
+  }
+
+  #imgproduct .img-show:hover {
+    background-position:0px 100%;
+
+  }
+</style>
 <div class="items-wrapper isotope row">
 
   @foreach($data as $item)
  
   <div class="item col-lg-3 col-6">
    <div class="item-inner animated flipInX">
-    <figure class="figure">
-     <img class="ducpanda-img"  src="{{$item->getImage()}}"/>
+    <figure class="figure" id="imgproduct">
+     <a class="img-show"></a>
    </figure>
    <div class="content text-left">
      <h3 class="sub-title"><a href="#">{{ $item->name}}</a></h3>
